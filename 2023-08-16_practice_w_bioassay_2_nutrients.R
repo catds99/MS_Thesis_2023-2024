797237c0cbb8d0eacc415a3d508d734df4bf5131
catds99 <schlenkc@email.sc.edu>
2023-08-16 14:06:56 -0400

    practice with bioassay 2 nutrients

---
+++ 2023-08-16_practice_w_bioassay_2_nutrients.R
@@ -0,0 +1,78 @@
+#2023-08-16
+#practice with bioassay 2 nutrients
+
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+library(readxl)
+library(ggplot2)
+library(car)
+library(rstatix)
+
+install.packages("ggpubr")
+library(ggpubr)
+
+#import data
+
+bioassay_2_nut = read_excel("data/bioassay_2_nutrients.xlsx")
+
+summary(bioassay_2_nut)
+glimpse(bioassay_2_nut)
+
+#find average per treatment
+
+bioassay_2_nut_avg = bioassay_2_nut %>%
+  group_by(Sample_ID) %>%
+  summarise(mean_NO3 = mean(NO3), mean_NO2 = mean(NO2), mean_NH3 = mean(NH3), mean_PO4 = mean(PO4)) 
+
+bioassay_2_nut_avg
+
+#bar graph:
+
+bioassay_2_NO3 = ggplot() +
+  geom_boxplot(aes(x = fct_relevel(Sample_ID, "T0", "Control", "DIN", "LP", "HP", "DIN_LP", "DIN_HP"), 
+                   y = NO3),
+               fill = "darkgrey",
+               data = bioassay_2_nut) +
+  xlab("Treatment") +
+  ylab(expression(paste("Concentration of NO3 (\u03BCM)"))) +
+  scale_x_discrete(labels= c("T0" = "Time Zero", "Control" = "Control", "DIN" = "DIN", "LP" = "LP", "HP" = "HP", "DIN_LP" = "DIN + LP", "DIN_HP" = "DIN + HP")) +
+  theme_classic() 
+ggsave(bioassay_2_NO3, filename = "figures/bioassay_2_NO3.png",
+       device = "png", height = 7, width = 11)
+
+bioassay_2_NO2 = ggplot() +
+  geom_boxplot(aes(x = fct_relevel(Sample_ID, "T0", "Control", "DIN", "LP", "HP", "DIN_LP", "DIN_HP"), 
+                   y = NO2),
+               fill = "darkgrey",
+               data = bioassay_2_nut) +
+  xlab("Treatment") +
+  ylab(expression(paste("Concentration of NO2 (\u03BCM)"))) +
+  scale_x_discrete(labels= c("T0" = "Time Zero", "Control" = "Control", "DIN" = "DIN", "LP" = "LP", "HP" = "HP", "DIN_LP" = "DIN + LP", "DIN_HP" = "DIN + HP")) +
+  theme_classic() 
+ggsave(bioassay_2_NO2, filename = "figures/bioassay_2_NO2.png",
+       device = "png", height = 7, width = 11)
+
+bioassay_2_NH4 = ggplot() +
+  geom_boxplot(aes(x = fct_relevel(Sample_ID, "T0", "Control", "DIN", "LP", "HP", "DIN_LP", "DIN_HP"), 
+                   y = NH3),
+               fill = "darkgrey",
+               data = bioassay_2_nut) +
+  xlab("Treatment") +
+  ylab(expression(paste("Concentration of NH4 (\u03BCM)"))) +
+  scale_x_discrete(labels= c("T0" = "Time Zero", "Control" = "Control", "DIN" = "DIN", "LP" = "LP", "HP" = "HP", "DIN_LP" = "DIN + LP", "DIN_HP" = "DIN + HP")) +
+  theme_classic() 
+ggsave(bioassay_2_NH4, filename = "figures/bioassay_2_NH4.png",
+       device = "png", height = 7, width = 11)
+
+bioassay_2_PO4 = ggplot() +
+  geom_boxplot(aes(x = fct_relevel(Sample_ID, "T0", "Control", "DIN", "LP", "HP", "DIN_LP", "DIN_HP"), 
+                   y = PO4),
+               fill = "darkgrey",
+               data = bioassay_2_nut) +
+  xlab("Treatment") +
+  ylab(expression(paste("Concentration of PO4 (\u03BCM)"))) +
+  scale_x_discrete(labels= c("T0" = "Time Zero", "Control" = "Control", "DIN" = "DIN", "LP" = "LP", "HP" = "HP", "DIN_LP" = "DIN + LP", "DIN_HP" = "DIN + HP")) +
+  theme_classic() 
+ggsave(bioassay_2_PO4, filename = "figures/bioassay_2_PO4.png",
+       device = "png", height = 7, width = 11)
