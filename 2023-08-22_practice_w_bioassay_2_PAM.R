476fb770958a50b869f620467a33cadd0b56b8c8
catds99 <schlenkc@email.sc.edu>
2023-08-22 13:44:23 -0400

    adding bioassay 2 PAM results

---
+++ 2023-08-22_practice_w_bioassay_2_PAM.R
@@ -0,0 +1,39 @@
+#2023-008-22
+#practice with PAM data - Bioassay 2 (06/06-08/23)
+
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+library(readxl)
+library(ggplot2)
+library(car)
+library(rstatix)
+
+#import data
+
+bioassay_2_PAM = read_excel("data/2023-06-08_bioassay_2.xlsx", sheet = "PAM")
+
+summary(bioassay_2_PAM)
+glimpse(bioassay_2_PAM)
+
+#find average per treatment
+
+bioassay_2_PAM_avg = bioassay_2_PAM %>%
+  group_by(Other) %>%
+  summarise(mean_FvFm = mean(FvFm))
+
+bioassay_2_PAM_avg
+
+#boxplot5
+
+bioassay_2_fig_5 = ggplot() +
+  geom_boxplot(aes(x = fct_relevel(Other, "T_0", "Control", "DIN", "LP", "HP", "DIN_LP", "DIN_HP"), 
+                   y = FvFm),
+               fill = "darkgrey",
+               data = bioassay_2_PAM) +
+  xlab("Treatment") +
+  ylab(expression(F[v]/F[m])) +
+  scale_x_discrete(labels= c("T_0" = "Time Zero", "Control" = "Control", "DIN" = "DIN", "LP" = "LP", "HP" = "HP", "DIN_LP" = "DIN + LP", "DIN_HP" = "DIN + HP")) +
+  theme_classic() 
+ggsave(bioassay_2_fig_5, filename = "figures/bioassay_2_fig_5.png",
+       device = "png", height = 7, width = 11)
