5b2ead45193bac05538d83f0f6ccaddcd63a8cba
catds99 <schlenkc@email.sc.edu>
2023-10-13 15:59:12 -0400

    fixing nutrient data

---
+++ 2023-10-11_exceedances.R
@@ -0,0 +1,29 @@
+#graphing DIN:DIP > 25 exceedances in NI
+#2023-10-11
+
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+library(readxl)
+library(ggplot2)
+library(car)
+library(rstatix)
+
+install.packages("ggpubr")
+library(ggpubr)
+
+#import data
+
+clambank_nutrients = read_excel("data/Clambank 3d Data.xlsx", sheet = "DINDIP25")
+
+summary(clambank_nutrients)
+glimpse(clambank_nutrients)
+
+#plot exceedances
+
+DIN_to_DIP_exceedances = ggplot(data = clambank_nutrients) +
+  geom_line(aes(x = Year, y = Percent_of_samples)) +
+  labs(x = "Year", y = "Percent of samples with DIN:DIP > 25") +
+  theme_classic()
+ggsave(DIN_to_DIP_exceedances, filename = "figures/DIN_to_DIP_exceedances.png",
+       device = "png", height = 7, width = 11)
